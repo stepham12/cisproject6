73636e1bbeb7cd0875ca37511fbfe1e57f72b318
Stephanie Amo <amost42@gmail.com>
2019-12-10 19:05:06 -0500

    Add filtering functionality.

--- cis237-assignment6/Controllers/BeveragesController.cs
+++ cis237-assignment6/Controllers/BeveragesController.cs
@@ -18,7 +18,72 @@ namespace cis237_assignment6.Controllers
         // GET: Beverages
         public ActionResult Index()
         {
-            return View(db.Beverages.ToList());
+            // Setup a variable to hold the cars data
+            DbSet<Beverage> BeveragesToFilter = db.Beverages;
+
+            // Setup some strings to hold the data that might be
+            // in the session. 
+            string filterName = "";
+            string filterPack = "";
+            string filterMin = "";
+            string filterMax = "";
+            // Define a min and max for the price
+            int min = 0;
+            int max = 100;
+
+
+            // Check to see if there is a value in the session,
+            // and if there is, assign it to the variable that
+            // we setup to hold the value.
+            if (!String.IsNullOrWhiteSpace(
+                (string)Session["s_name"]
+            ))
+            {
+                filterName = (string)Session["s_name"];
+            }
+            if (!String.IsNullOrWhiteSpace(
+                (string)Session["s_pack"]
+            ))
+            {
+                filterPack = (string)Session["s_pack"];
+            }
+            if (!String.IsNullOrWhiteSpace(
+                (string)Session["s_min"]
+            ))
+            {
+                filterMin = (string)Session["s_min"];
+                min = Int32.Parse(filterMin);
+            }
+            if (!String.IsNullOrWhiteSpace(
+                (string)Session["s_max"]
+            ))
+            {
+                filterMax = (string)Session["s_max"];
+                max = Int32.Parse(filterMax);
+            }
+
+            // Do the filter on the BeveragesToFilter Dataset.
+            IEnumerable<Beverage> filtered = BeveragesToFilter.Where(
+                car => car.price >= min &&
+                car.price <= max &&
+                car.name.Contains(filterName) &&
+                car.pack.Contains(filterPack)
+            );
+
+            // Convert the dataset to a list now that the query
+            // work is done on it. 
+            IList<Beverage> finalFiltered = filtered.ToList();
+
+            // Place the string representation of the values
+            // that are in the session into the viewbag so
+            // that they can be retrieved and displayed in the View.
+            ViewBag.filterName = filterName;
+            ViewBag.filterPack = filterPack;
+            ViewBag.filterMin = filterMin;
+            ViewBag.filterMax = filterMax;
+
+            // Return the view with the filtered selection of beverages
+            return View(finalFiltered);
         }
 
         // GET: Beverages/Details/5
@@ -113,6 +178,28 @@ namespace cis237_assignment6.Controllers
             Beverage beverage = db.Beverages.Find(id);
             db.Beverages.Remove(beverage);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Method to take in the filter data and store it in
+        // the session. Will get used in the index method.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Filter()
+        {
+            // Get the form data that we sent out for the request object.
+            string name = Request.Form.Get("name");
+            string pack = Request.Form.Get("pack");
+            string min = Request.Form.Get("min");
+            string max = Request.Form.Get("max");
+
+            // put it into the session so that other methods can have access to it.
+            Session["s_name"] = name;
+            Session["s_pack"] = pack;
+            Session["s_min"] = min;
+            Session["s_max"] = max;
+
+            // Redirect to the index page
             return RedirectToAction("Index");
         }
 
